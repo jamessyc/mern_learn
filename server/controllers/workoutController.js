94466b6bca62ea178b5021880431d2cdde1a98e0
c0werd <james.sy.coward@gmail.com>
2023-06-05 23:24:06 +0100

    auth tut #17 assigning workouts to users

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose')
 
 // get all workouts
 const getWorkouts = async (req, res) => {
-    const workouts = await Workout.find({}).sort({createdAt: -1})
+    const user_id = req.user._id
+
+    const workouts = await Workout.find({ user_id }).sort({createdAt: -1})
     
     res.status(200).json(workouts)
 
@@ -50,7 +52,8 @@ const createWorkout = async (req, res) => {
 
     // add doc to DB
     try {
-        const workout = await Workout.create({title, load, reps})
+        const user_id = req.user._id
+        const workout = await Workout.create({title, load, reps, user_id})
         res.status(200).json(workout)
 
     } catch (error) {
